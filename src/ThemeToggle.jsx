e0322c1b1bd304745333607bc29466fa55c0fc89
MAC-ANTHONY <macanthonygreat@gmail.com>
2023-10-02 04:57:46 +0100

    done

--- src/ThemeToggle.jsx
+++ src/ThemeToggle.jsx
@@ -5,6 +5,7 @@ const ThemeToggle = () => {
   const { isDarkTheme, toggleDarkTheme } = useGlobalContext()
   return (
     <section className='toggle-container'>
+      
       <button className='dark-toggle' onClick={toggleDarkTheme}>
         {isDarkTheme ? (
           <BsFillMoonFill className='toggle-icon' />
