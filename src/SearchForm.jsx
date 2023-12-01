e972ec755b3faa2b541b2360cf5836fe37ee9162
MAC-ANTHONY <macanthonygreat@gmail.com>
2023-12-01 23:39:37 +0000

    new update

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,27 +1,28 @@
-import { useGlobalContext } from "./context"
+import { useGlobalContext } from "./context";
 
 const SearchForm = () => {
-  const { setSearchTerm } = useGlobalContext()
+  const { setSearchTerm } = useGlobalContext();
   const handleSubmit = (e) => {
-    e.preventDefault()
-    const searchValue = e.target.elements.search.value
-    if (!searchValue) return
-    setSearchTerm(searchValue)
-  }
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    setSearchTerm(searchValue);
+  };
 
   return (
     <section>
-      <h1 className='title'>Unsplash images</h1>
+      <h1 className="title">Unsplash images</h1>
       <form
-        className='search-form'
+        className="search-form"
         onSubmit={handleSubmit}
-        style={{ color: "black" }}>
-        <input type='text' className='form-input search-input' name='search' />
-        <button type='submit' className='btn'>
+        style={{ color: "black" }}
+      >
+        <input type="text" className="form-input search-input" name="search" />
+        <button type="submit" className="btn">
           search
         </button>
       </form>
     </section>
-  )
-}
-export default SearchForm
+  );
+};
+export default SearchForm;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import "./index.css"
-import { AppProvider } from "./context"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { AppProvider } from "./context";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AppProvider>
@@ -15,4 +15,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </AppProvider>
-)
+);
